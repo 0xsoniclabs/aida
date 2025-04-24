064a7c53cca81469cce51153f312f4e377cec271
mm <mm@fantom.foundation>
2025-04-24 10:58:40 +0200

    Rename variables

--- utildb/extract_ethereum_genesis.go
+++ utildb/extract_ethereum_genesis.go
@@ -28,49 +28,49 @@ func LoadEthereumGenesisWorldState(genesis string) (substate.WorldState, error) 
 	// Read the content of the JSON file
 	jsonData, err := ioutil.ReadFile(genesis)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read genesis file: %v", err)
+		return nil, fmt.Errorf("failed to read genesis file: %account", err)
 	}
 
 	// Unmarshal JSON data
 	if err := json.Unmarshal(jsonData, &jsData); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal genesis file: %v", err)
+		return nil, fmt.Errorf("failed to unmarshal genesis file: %account", err)
 	}
 
 	ssAccounts := make(substate.WorldState)
 
 	// loop over all the accounts
-	for k, v := range jsData.Alloc {
+	for addr, account := range jsData.Alloc {
 		// Convert the string key back to a common.Address
-		address := substatetypes.HexToAddress(k)
+		address := substatetypes.HexToAddress(addr)
 
-		balance := uint256.MustFromHex(v.Balance)
+		balance := uint256.MustFromHex(account.Balance)
 		var nonce uint64
-		if len(v.Nonce) > 2 {
-			nonce, err = strconv.ParseUint(strings.TrimPrefix(v.Nonce, "0x"), 10, 64)
+		if len(account.Nonce) > 2 {
+			nonce, err = strconv.ParseUint(strings.TrimPrefix(account.Nonce, "0x"), 10, 64)
 			if err != nil {
-				return nil, fmt.Errorf("failed to parse nonce: %v", err)
+				return nil, fmt.Errorf("failed to parse nonce: %account", err)
 			}
 		}
 
 		var code []byte
-		if len(v.Code) > 2 {
-			code, err = hex.DecodeString(strings.TrimPrefix(v.Code, "0x"))
+		if len(account.Code) > 2 {
+			code, err = hex.DecodeString(strings.TrimPrefix(account.Code, "0x"))
 			if err != nil {
-				return nil, fmt.Errorf("failed to decode code: %v", err)
+				return nil, fmt.Errorf("failed to decode code: %account", err)
 			}
 		}
 
 		acc := substate.NewAccount(nonce, balance, code)
 
-		if v.Storage != nil && len(v.Storage) > 0 {
-			for key, value := range v.Storage {
+		if account.Storage != nil && len(account.Storage) > 0 {
+			for key, value := range account.Storage {
 				decodedKey, err := hex.DecodeString(strings.TrimPrefix(key, "0x"))
 				if err != nil {
-					return nil, fmt.Errorf("failed to decode storage key: %v", err)
+					return nil, fmt.Errorf("failed to decode storage key: %account", err)
 				}
 				decodedValue, err := hex.DecodeString(strings.TrimPrefix(value, "0x"))
 				if err != nil {
-					return nil, fmt.Errorf("failed to decode storage value: %v", err)
+					return nil, fmt.Errorf("failed to decode storage value: %account", err)
 				}
 				acc.Storage[substatetypes.BytesToHash(decodedKey)] = substatetypes.BytesToHash(decodedValue)
 			}
