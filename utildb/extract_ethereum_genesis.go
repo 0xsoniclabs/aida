e098b626b0273f75fac8533e25ad0143b67ab182
mm <mm@fantom.foundation>
2025-04-24 10:53:11 +0200

    Convert json struct to struct

--- utildb/extract_ethereum_genesis.go
+++ utildb/extract_ethereum_genesis.go
@@ -13,9 +13,18 @@ import (
 	"github.com/holiman/uint256"
 )
 
+type GenesisData struct {
+	Alloc map[string]struct {
+		Balance string            `json:"balance,omitempty"`
+		Nonce   string            `json:"nonce,omitempty"`
+		Code    string            `json:"code,omitempty"`
+		Storage map[string]string `json:"storage,omitempty"`
+	} `json:"alloc"`
+}
+
 // LoadEthereumGenesisWorldState loads opera initial world state from worldstate-db as WorldState
 func LoadEthereumGenesisWorldState(genesis string) (substate.WorldState, error) {
-	var jsData map[string]interface{}
+	var jsData GenesisData
 	// Read the content of the JSON file
 	jsonData, err := ioutil.ReadFile(genesis)
 	if err != nil {
@@ -27,33 +36,25 @@ func LoadEthereumGenesisWorldState(genesis string) (substate.WorldState, error) 
 		return nil, fmt.Errorf("failed to unmarshal genesis file: %v", err)
 	}
 
-	// get field alloc
-	alloc, ok := jsData["alloc"]
-	if !ok {
-		return nil, fmt.Errorf("failed to get alloc field from genesis file")
-	}
-
 	ssAccounts := make(substate.WorldState)
 
 	// loop over all the accounts
-	for k, v := range alloc.(map[string]interface{}) {
+	for k, v := range jsData.Alloc {
 		// Convert the string key back to a common.Address
 		address := substatetypes.HexToAddress(k)
 
-		balance := uint256.MustFromHex(v.(map[string]interface{})["balance"].(string))
+		balance := uint256.MustFromHex(v.Balance)
 		var nonce uint64
-		nonceS, ok := v.(map[string]interface{})["nonce"].(string)
-		if ok {
-			nonce, err = strconv.ParseUint(strings.TrimPrefix(nonceS, "0x"), 10, 64)
+		if len(v.Nonce) > 2 {
+			nonce, err = strconv.ParseUint(strings.TrimPrefix(v.Nonce, "0x"), 10, 64)
 			if err != nil {
 				return nil, fmt.Errorf("failed to parse nonce: %v", err)
 			}
 		}
 
 		var code []byte
-		codeS, ok := v.(map[string]interface{})["code"].(string)
-		if ok {
-			code, err = hex.DecodeString(strings.TrimPrefix(codeS, "0x"))
+		if len(v.Code) > 2 {
+			code, err = hex.DecodeString(strings.TrimPrefix(v.Code, "0x"))
 			if err != nil {
 				return nil, fmt.Errorf("failed to decode code: %v", err)
 			}
@@ -61,14 +62,13 @@ func LoadEthereumGenesisWorldState(genesis string) (substate.WorldState, error) 
 
 		acc := substate.NewAccount(nonce, balance, code)
 
-		storageMap, ok := v.(map[string]interface{})["storage"]
-		if ok {
-			for key, value := range storageMap.(map[string]interface{}) {
+		if v.Storage != nil && len(v.Storage) > 0 {
+			for key, value := range v.Storage {
 				decodedKey, err := hex.DecodeString(strings.TrimPrefix(key, "0x"))
 				if err != nil {
 					return nil, fmt.Errorf("failed to decode storage key: %v", err)
 				}
-				decodedValue, err := hex.DecodeString(strings.TrimPrefix(value.(string), "0x"))
+				decodedValue, err := hex.DecodeString(strings.TrimPrefix(value, "0x"))
 				if err != nil {
 					return nil, fmt.Errorf("failed to decode storage value: %v", err)
 				}
