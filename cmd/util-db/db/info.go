8025b8fdc4e371a8e562843e02a454cee152d927
mm <mm@fantom.foundation>
2025-05-31 09:19:27 +0200

    Fix substate encodings

--- cmd/util-db/db/info.go
+++ cmd/util-db/db/info.go
@@ -116,7 +116,7 @@ func printCount(ctx *cli.Context) error {
 	// print substate count
 	if dbComponent == dbcomponent.Substate || dbComponent == dbcomponent.All {
 		sdb := db.MakeDefaultSubstateDBFromBaseDB(base)
-		err = sdb.SetSubstateEncoding(db.SubstateEncodingSchema(cfg.SubstateEncoding))
+		err = sdb.SetSubstateEncoding(cfg.SubstateEncoding)
 		if err != nil {
 			return fmt.Errorf("cannot set substate encoding; %w", err)
 		}
@@ -177,7 +177,7 @@ func printRange(ctx *cli.Context) error {
 		if err != nil {
 			return fmt.Errorf("cannot open aida-db; %w", err)
 		}
-		err = sdb.SetSubstateEncoding(db.SubstateEncodingSchema(cfg.SubstateEncoding))
+		err = sdb.SetSubstateEncoding(cfg.SubstateEncoding)
 		if err != nil {
 			return fmt.Errorf("cannot set substate encoding; %w", err)
 		}
