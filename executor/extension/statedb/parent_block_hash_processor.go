2b941c4466ca412a151fbb70ab931c1ea916829b
cabrador <ph@fantom.foundation>
2025-06-02 10:33:26 +0000

    Add parent block hash processor for executing prague time

---
+++ executor/extension/statedb/parent_block_hash_processor.go
@@ -0,0 +1,67 @@
+package statedb
+
+import (
+	"fmt"
+	"github.com/0xsoniclabs/aida/executor"
+	"github.com/0xsoniclabs/aida/executor/extension"
+	"github.com/0xsoniclabs/aida/txcontext"
+	"github.com/0xsoniclabs/aida/utils"
+	"github.com/0xsoniclabs/sonic/evmcore"
+	"github.com/ethereum/go-ethereum/core/vm"
+	"math/big"
+)
+
+func NewParentBlockHashProcessor(cfg *utils.Config) executor.Extension[txcontext.TxContext] {
+	return &parentBlockHashProcessor{
+		cfg:          cfg,
+		NilExtension: extension.NilExtension[txcontext.TxContext]{},
+	}
+}
+
+type parentBlockHashProcessor struct {
+	cfg          *utils.Config
+	hashProvider utils.StateHashProvider
+	extension.NilExtension[txcontext.TxContext]
+}
+
+func (p *parentBlockHashProcessor) PreRun(_ executor.State[txcontext.TxContext], ctx *executor.Context) error {
+	p.hashProvider = utils.MakeStateHashProvider(ctx.AidaDb)
+	return nil
+}
+
+// PreBlock processes parent block hash.
+func (p *parentBlockHashProcessor) PreBlock(state executor.State[txcontext.TxContext], ctx *executor.Context) error {
+	// We are saving historic block hashes, first block must be skipped because
+	// there is no history at this point
+	if uint64(state.Block) == utils.KeywordBlocks[p.cfg.ChainID]["first"] {
+		return nil
+	}
+
+	inputEnv := state.Data.GetBlockEnvironment()
+	chainCfg, err := p.cfg.GetChainConfig(inputEnv.GetFork())
+	if err != nil {
+		return fmt.Errorf("cannot get chain config: %w", err)
+	}
+
+	if !chainCfg.IsPrague(new(big.Int).SetUint64(inputEnv.GetNumber()), inputEnv.GetTimestamp()) {
+		return nil
+	}
+
+	prevBlockHash, err := p.hashProvider.GetStateHash(state.Block - 1)
+	if err != nil {
+		return fmt.Errorf("cannot get previous block hash: %w", err)
+	}
+
+	var hashError error
+	blockCtx := utils.PrepareBlockCtx(inputEnv, &hashError)
+	evm := vm.NewEVM(*blockCtx, ctx.State, chainCfg, p.cfg.VmCfg)
+	evmcore.ProcessParentBlockHash(
+		prevBlockHash,
+		evm,
+	)
+
+	if hashError != nil {
+		return fmt.Errorf("hash error while processing parent block hash: %v", err)
+	}
+	return nil
+}
---
+++ utils/block_context.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"github.com/0xsoniclabs/aida/txcontext"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/vm"
+	"math/big"
+)
+
+// PrepareBlockCtx creates a block context for evm call from given BlockEnvironment.
+func PrepareBlockCtx(inputEnv txcontext.BlockEnvironment, hashError *error) *vm.BlockContext {
+	getHash := func(num uint64) common.Hash {
+		var h common.Hash
+		h, *hashError = inputEnv.GetBlockHash(num)
+		return h
+	}
+
+	blockCtx := &vm.BlockContext{
+		CanTransfer: core.CanTransfer,
+		Transfer:    core.Transfer,
+		Coinbase:    inputEnv.GetCoinbase(),
+		BlockNumber: new(big.Int).SetUint64(inputEnv.GetNumber()),
+		Time:        inputEnv.GetTimestamp(),
+		Difficulty:  inputEnv.GetDifficulty(),
+		Random:      inputEnv.GetRandom(),
+		GasLimit:    inputEnv.GetGasLimit(),
+		GetHash:     getHash,
+	}
+	// If currentBaseFee is defined, add it to the vmContext.
+	baseFee := inputEnv.GetBaseFee()
+	if baseFee != nil {
+		blockCtx.BaseFee = new(big.Int).Set(baseFee)
+	}
+
+	blobBaseFee := inputEnv.GetBlobBaseFee()
+	if blobBaseFee != nil {
+		blockCtx.BlobBaseFee = new(big.Int).Set(blobBaseFee)
+	}
+	return blockCtx
+}
