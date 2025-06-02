e420cfe64c323b06fc8c2c4c6e68fd0102cfd1f6
cabrador <ph@fantom.foundation>
2025-06-02 10:33:26 +0000

    Add parent block hash processor for executing prague time

---
+++ executor/extension/statedb/parent_block_hash_processor_test.go
@@ -0,0 +1,64 @@
+package statedb
+
+import (
+	"github.com/0xsoniclabs/aida/executor"
+	"github.com/0xsoniclabs/aida/executor/extension"
+	"github.com/0xsoniclabs/aida/state"
+	"github.com/0xsoniclabs/aida/txcontext"
+	substateCtx "github.com/0xsoniclabs/aida/txcontext/substate"
+	"github.com/0xsoniclabs/aida/utils"
+	"github.com/0xsoniclabs/substate/substate"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+	"math"
+	"testing"
+)
+
+func TestParentBlockHashProcessor_PreBlock(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockProvider := utils.NewMockStateHashProvider(ctrl)
+	mockState := state.NewMockStateDB(ctrl)
+	hash := common.Hash{123}
+	// All actions are called only once as only one PreBlock passes conditions
+	gomock.InOrder(
+		mockProvider.EXPECT().GetStateHash(2).Return(hash, nil),
+		mockState.EXPECT().AddAddressToAccessList(gomock.Any()),
+		mockState.EXPECT().Snapshot().Return(1),
+		mockState.EXPECT().Exist(gomock.Any()).Return(false),
+		mockState.EXPECT().Finalise(gomock.Any()),
+	)
+
+	hashProcessor := parentBlockHashProcessor{
+		// At the time of implementation, Sonic does not have Prague time yet
+		cfg:          utils.NewTestConfig(t, utils.HoleskyChainID, 1, 10, false, "Prague"),
+		hashProvider: mockProvider,
+		NilExtension: extension.NilExtension[txcontext.TxContext]{},
+	}
+
+	// First call is skipped because block number is the first block number of given chain id
+	err := hashProcessor.PreBlock(executor.State[txcontext.TxContext]{Block: int(utils.KeywordBlocks[utils.HoleskyChainID]["first"]), Data: substateCtx.NewTxContext(&substate.Substate{
+		Env:   &substate.Env{Timestamp: math.MaxUint64},
+		Block: utils.KeywordBlocks[utils.HoleskyChainID]["first"],
+	})}, &executor.Context{State: mockState})
+	require.NoError(t, err, "PreBlock failed")
+
+	chainCfg, err := hashProcessor.cfg.GetChainConfig("Prague")
+	require.NoError(t, err, "GetChainConfig failed")
+
+	// Second call is skipped because it does not have prague time yet
+	err = hashProcessor.PreBlock(executor.State[txcontext.TxContext]{Block: 2, Data: substateCtx.NewTxContext(&substate.Substate{
+		Env:   &substate.Env{Timestamp: *chainCfg.PragueTime - 100},
+		Block: 2,
+	})}, &executor.Context{State: mockState})
+	require.NoError(t, err, "PreBlock failed")
+
+	// Third call calls the StateHashProvider with block-1
+	err = hashProcessor.PreBlock(executor.State[txcontext.TxContext]{Block: 3, Data: substateCtx.NewTxContext(&substate.Substate{
+		Env:         &substate.Env{Timestamp: math.MaxUint64},
+		Result:      nil,
+		Block:       3,
+		Transaction: 0,
+	})}, &executor.Context{State: mockState})
+	require.NoError(t, err, "PreBlock failed")
+}
