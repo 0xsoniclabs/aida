ef51db198f7e856587dc5a3fb31de5316fc2f67b
wsodsong <wasuwee.sodsong@gmail.com>
2025-06-19 19:11:03 +0800

    Rename hash provider and add unit tests

--- utildb/info.go
+++ utildb/info.go
@@ -139,7 +139,7 @@ func GetUpdateCount(cfg *utils.Config, database db.BaseDB) (uint64, error) {
 func GetStateHashCount(cfg *utils.Config, database db.BaseDB) (uint64, error) {
 	var count uint64
 
-	hashProvider := utils.MakeStateHashProvider(database)
+	hashProvider := utils.MakeHashProvider(database)
 	for i := cfg.First; i <= cfg.Last; i++ {
 		_, err := hashProvider.GetStateRootHash(int(i))
 		if err != nil {
