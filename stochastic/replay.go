2212f7b3af88644703805c55a0b59f9446f82cf9
Ubuntu <Bernhard Scholz>
2025-06-23 15:36:55 +1000

    Missing Implementation of Transient Storage Operation in Stochastic Replay
    This PR adds the two operations GetTransientState and SetTransientState to the stochastic replayer.
    Recently, operations were added to operations that were not sorted alphabetically. This PR sorts
    all maps related to StateDB operations as well.

--- stochastic/replay.go
+++ stochastic/replay.go
@@ -408,6 +408,9 @@ func (ss *stochasticState) execute(op int, addrCl int, keyCl int, valueCl int) {
 	case GetStorageRootID:
 		db.GetStorageRoot(addr)
 
+	case GetTransientStateID:
+		db.GetState(addr, key)
+
 	case HasSelfDestructedID:
 		db.HasSelfDestructed(addr)
 
@@ -432,6 +435,18 @@ func (ss *stochasticState) execute(op int, addrCl int, keyCl int, valueCl int) {
 			db.RevertToSnapshot(snapshot)
 		}
 
+	case SelfDestructID:
+		db.SelfDestruct(addr)
+		if idx := find(ss.selfDestructed, addrIdx); idx == -1 {
+			ss.selfDestructed = append(ss.selfDestructed, addrIdx)
+		}
+
+	case SelfDestruct6780ID:
+		db.SelfDestruct6780(addr)
+		if idx := find(ss.selfDestructed, addrIdx); idx == -1 {
+			ss.selfDestructed = append(ss.selfDestructed, addrIdx)
+		}
+
 	case SetCodeID:
 		sz := rg.Intn(MaxCodeSize-1) + 1
 		if ss.traceDebug {
@@ -450,6 +465,9 @@ func (ss *stochasticState) execute(op int, addrCl int, keyCl int, valueCl int) {
 
 	case SetStateID:
 		db.SetState(addr, key, value)
+
+	case SetTransientStateID:
+		db.SetTransientState(addr, key, value)
 
 	case SnapshotID:
 		id := db.Snapshot()
@@ -475,21 +493,8 @@ func (ss *stochasticState) execute(op int, addrCl int, keyCl int, valueCl int) {
 			}
 			db.SubBalance(addr, uint256.NewInt(value), 0)
 		}
-
-	case SelfDestructID:
-		db.SelfDestruct(addr)
-		if idx := find(ss.selfDestructed, addrIdx); idx == -1 {
-			ss.selfDestructed = append(ss.selfDestructed, addrIdx)
-		}
-
-	case SelfDestruct6780ID:
-		db.SelfDestruct6780(addr)
-		if idx := find(ss.selfDestructed, addrIdx); idx == -1 {
-			ss.selfDestructed = append(ss.selfDestructed, addrIdx)
-		}
-
 	default:
-		ss.log.Fatal("invalid operation")
+		ss.log.Fatal("invalid operation "+opText[op])
 	}
 }
 
