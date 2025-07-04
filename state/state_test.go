2a2ed2a197fe57c725ce9ac1e262d71f43e79551
Herbert Jordan <herbert@swrnd.ch>
2025-07-04 09:13:48 +0200

    Reconstructed issue detected in Sepolia block 4.9M

--- state/state_test.go
+++ state/state_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/tracing"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/stretchr/testify/require"
 )
@@ -35,41 +36,86 @@ func runSepoliaIssue(t *testing.T, state StateDB) {
 	key := common.Hash{4, 5, 6}
 	value := common.Hash{7, 8, 9}
 
+	// We start by creating an account with some non-empty state.
 	state.BeginBlock(1)
-	// Create an non-empty account.
-	state.BeginTransaction(1)
-	state.CreateAccount(address)
-	state.CreateContract(address)
-	state.SetState(address, key, value)
-	state.EndTransaction()
+	{
+		state.BeginTransaction(1)
+		{
+			state.CreateAccount(address)
+			state.CreateContract(address)
+			state.SetNonce(address, 1, tracing.NonceChangeUnspecified)
+			state.SetState(address, key, value)
+		}
+		state.EndTransaction()
+	}
 	state.EndBlock()
 
-	require.False(t, evmCreateCheckIsEmpty(state, address), "Account should not be empty after creation")
-
+	// In the next block we have two transactions:
 	state.BeginBlock(2)
-	// Destroy the account.
-	state.BeginTransaction(1)
-	state.SelfDestruct(address)
-	state.EndTransaction()
+	{
+		// 1. Self-destruct the account.
+		state.BeginTransaction(1)
+		{
+			// At this point, the account storage is not empty.
+			require.False(t, isEmptyStorageRoot(state.GetStorageRoot(address)))
+			require.Equal(t, uint64(1), state.GetNonce(address))
+			state.SelfDestruct(address)
 
-	// Create the same account again.
-	state.BeginTransaction(2)
-	state.CreateAccount(address)
-	state.CreateContract(address)
-	state.SetState(address, key, value)
-	state.EndTransaction()
+			// The effects of the self-destruct are only visible after the end
+			// of the transaction. So the storage root is still not empty.
+			require.False(t, isEmptyStorageRoot(state.GetStorageRoot(address)))
+			require.Equal(t, uint64(1), state.GetNonce(address))
+
+		}
+		state.EndTransaction()
+
+		// 2. Create the same account again.
+		state.BeginTransaction(2)
+		{
+			// The effects of the self-destruct are now visible. The storage
+			// root should be empty and the nonce should be reset to 0.
+			require.True(t, isEmptyStorageRoot(state.GetStorageRoot(address)))
+			require.Equal(t, uint64(0), state.GetNonce(address))
+
+			state.CreateAccount(address)
+			state.CreateContract(address)
+
+			// The account is still empty, even after its re-creation.
+			require.True(t, isEmptyStorageRoot(state.GetStorageRoot(address)))
+			require.Equal(t, uint64(0), state.GetNonce(address))
+
+			// Writing to the storage does not change the storage root, since it
+			// is only re-evaluated at the end of the block.
+			state.SetState(address, key, value)
+			require.True(t, isEmptyStorageRoot(state.GetStorageRoot(address)))
+		}
+		state.EndTransaction()
+
+		// 3. The view on the re-born account after the end of the transaction.
+		state.BeginTransaction(3)
+		{
+			// Even here, the storage root remains empty, as the storage root is
+			// not re-evaluated until the end of the block.
+			require.True(t, isEmptyStorageRoot(state.GetStorageRoot(address)))
+		}
+		state.EndTransaction()
+
+	}
 	state.EndBlock()
 
-	require.False(t, evmCreateCheckIsEmpty(state, address), "Account should not be empty after re-creation")
+	// Check the state after the block.
+	state.BeginBlock(3)
+	{
+		state.BeginTransaction(1)
+		{
+			require.True(t, isEmptyStorageRoot(state.GetStorageRoot(address)))
+		}
+		state.EndTransaction()
+	}
+	state.EndBlock()
 }
 
-func evmCreateCheckIsEmpty(state StateDB, address common.Address) bool {
-	contractHash := state.GetCodeHash(address)
-	storageRoot := state.GetStorageRoot(address)
-	nonce := state.GetNonce(address)
-
-	// Check copied from evm.go create function.
-	return nonce != 0 ||
-		(contractHash != (common.Hash{}) && contractHash != types.EmptyCodeHash) || // non-empty code
-		(storageRoot != (common.Hash{}) && storageRoot != types.EmptyRootHash)
+func isEmptyStorageRoot(hash common.Hash) bool {
+	// The empty state root is defined as the hash of an empty Merkle trie.
+	return hash == common.Hash{} || hash == types.EmptyRootHash
 }
