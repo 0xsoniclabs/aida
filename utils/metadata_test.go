66881de28eef5f9294230c4d8879c0e26694d7b1
mm <mm@fantom.foundation>
2025-07-08 21:31:08 +0200

    remove import underscore

--- utils/metadata_test.go
+++ utils/metadata_test.go
@@ -23,7 +23,7 @@ import (
 	"github.com/0xsoniclabs/substate/db"
 	"github.com/0xsoniclabs/substate/substate"
 	"github.com/Fantom-foundation/lachesis-base/common/bigendian"
-	geth_leveldb "github.com/ethereum/go-ethereum/ethdb/leveldb"
+	gethleveldb "github.com/ethereum/go-ethereum/ethdb/leveldb"
 	"github.com/stretchr/testify/assert"
 	"github.com/syndtr/goleveldb/leveldb"
 	"go.uber.org/mock/gomock"
@@ -1143,7 +1143,7 @@ func TestHasStateHashPatch(t *testing.T) {
 	})
 
 	t.Run("case exist", func(t *testing.T) {
-		eDb, err := geth_leveldb.New(path, 1024, 100, "profiling", false)
+		eDb, err := gethleveldb.New(path, 1024, 100, "profiling", false)
 		if err != nil {
 			t.Fatalf("failed to open leveldb: %v", err)
 		}
