4e3fb4bf9152f7a3ea914ed30f81eb64766b2a2a
Bernhard Scholz <bernhard@fantom.foundation>
2025-07-10 16:21:48 +1000

    Tidy up proxy

--- tracer/tracer_proxy.go
+++ tracer/tracer_proxy.go
@@ -29,152 +29,99 @@ import (
 	"github.com/holiman/uint256"
 )
 
-// TracerProxy data structure for capturing StateDB events
+// TracerProxy data structure for writing StateDB operations
 type TracerProxy struct {
-	db   state.StateDB      // real StateDB object
-	ctx  *ArgumentContext   // event ctx for deriving statistical parameters
+	db   state.StateDB      // StateDB object
+	ctx  *ArgumentContext   // context that keeps track of the argument history
+	                        // of previous StateDB operations
 }
 
-// NewTracerProxy creates a new StateDB proxy for recording events.
+// NewTracerProxy creates a new StateDB proxy for recording and writing events.
 func NewTracerProxy(db state.StateDB, ctx *ArgumentContext) *TracerProxy {
 	return &TracerProxy{
-		db:        db,
+		db:   db,
 		ctx:  ctx,
 	}
 }
 
-// CreateAccount creates a new account.
 func (p *TracerProxy) CreateAccount(address common.Address) {
-	// register event
-	p.ctx.WriteAddressOp(CreateAccountID, &address)
-
-	// call real StateDB
+	p.ctx.WriteAddressOp(CreateAccountID, &address, []byte{})
 	p.db.CreateAccount(address)
 }
 
-// SubBalance subtracts amount from a contract address.
 func (p *TracerProxy) SubBalance(address common.Address, amount *uint256.Int, reason tracing.BalanceChangeReason) uint256.Int {
-	// register event
-	p.ctx.WriteAddressOp(SubBalanceID, &address)
-
-	// call real StateDB
+	// TBD: find an encoding for amount and reason in the form of a byte sequence
+	data := []byte{} 
+	p.ctx.WriteAddressOp(SubBalanceID, &address, data)
 	return p.db.SubBalance(address, amount, reason)
 }
 
-// AddBalance adds amount to a contract address.
 func (p *TracerProxy) AddBalance(address common.Address, amount *uint256.Int, reason tracing.BalanceChangeReason) uint256.Int {
-	// register event
+	// TBD: find an encoding for amount and reason in the form of a byte sequence
+	data := []byte{} 
 	p.ctx.WriteAddressOp(AddBalanceID, &address)
-
-	// call real StateDB
 	return p.db.AddBalance(address, amount, reason)
 }
 
-// GetBalance retrieves the amount of a contract address.
 func (p *TracerProxy) GetBalance(address common.Address) *uint256.Int {
-	// register event
-	p.ctx.WriteAddressOp(GetBalanceID, &address)
-
-	// call real StateDB
+	p.ctx.WriteAddressOp(GetBalanceID, &address, []byte{})
 	return p.db.GetBalance(address)
 }
 
-// GetNonce retrieves the nonce of a contract address.
 func (p *TracerProxy) GetNonce(address common.Address) uint64 {
-	// register event
 	p.ctx.WriteAddressOp(GetNonceID, &address)
-
-	// call real StateDB
 	return p.db.GetNonce(address)
 }
 
-// SetNonce sets the nonce of a contract address.
 func (p *TracerProxy) SetNonce(address common.Address, nonce uint64, reason tracing.NonceChangeReason) {
-	// register event
 	p.ctx.WriteAddressOp(SetNonceID, &address)
-
-	// call real StateDB
 	p.db.SetNonce(address, nonce, reason)
 }
 
-// GetCodeHash returns the hash of the EVM bytecode.
 func (p *TracerProxy) GetCodeHash(address common.Address) common.Hash {
-	// register event
 	p.ctx.WriteAddressOp(GetCodeHashID, &address)
-
-	// call real StateDB
 	return p.db.GetCodeHash(address)
 }
 
-// GetCode returns the EVM bytecode of a contract.
 func (p *TracerProxy) GetCode(address common.Address) []byte {
-	// register event
 	p.ctx.WriteAddressOp(GetCodeID, &address)
-
-	// call real StateDB
 	return p.db.GetCode(address)
 }
 
-// SetCode sets the EVM bytecode of a contract.
 func (p *TracerProxy) SetCode(address common.Address, code []byte) []byte {
-	// register event
 	p.ctx.WriteAddressOp(SetCodeID, &address)
-
-	// call real StateDB
 	return p.db.SetCode(address, code)
 }
 
-// GetCodeSize returns the EVM bytecode's size.
 func (p *TracerProxy) GetCodeSize(address common.Address) int {
-	// register event
 	p.ctx.WriteAddressOp(GetCodeSizeID, &address)
-
-	// call real StateDB
 	return p.db.GetCodeSize(address)
 }
 
-// AddRefund adds gas to the refund counter.
 func (p *TracerProxy) AddRefund(gas uint64) {
-	// call real StateDB
 	p.db.AddRefund(gas)
 }
 
-// SubRefund subtracts gas to the refund counter.
 func (p *TracerProxy) SubRefund(gas uint64) {
-	// call real StateDB
 	p.db.SubRefund(gas)
 }
 
-// GetRefund returns the current value of the refund counter.
 func (p *TracerProxy) GetRefund() uint64 {
-	// call real StateDB
 	return p.db.GetRefund()
 }
 
-// GetCommittedState retrieves a value that is already committed.
 func (p *TracerProxy) GetCommittedState(address common.Address, key common.Hash) common.Hash {
-	// register event
 	p.ctx.WriteKeyOp(GetCommittedStateID, &address, &key)
-
-	// call real StateDB
 	return p.db.GetCommittedState(address, key)
 }
 
-// GetState retrieves a value from the StateDB.
 func (p *TracerProxy) GetState(address common.Address, key common.Hash) common.Hash {
-	// register event
 	p.ctx.WriteKeyOp(GetStateID, &address, &key)
-
-	// call real StateDB
 	return p.db.GetState(address, key)
 }
 
-// SetState sets a value in the StateDB.
 func (p *TracerProxy) SetState(address common.Address, key common.Hash, value common.Hash) common.Hash {
-	// register event
 	p.ctx.WriteValueOp(SetStateID, &address, &key, &value)
-
-	// call real StateDB
 	return p.db.SetState(address, key, value)
 }
 func (p *TracerProxy) SetTransientState(addr common.Address, key common.Hash, value common.Hash) {
@@ -187,133 +134,73 @@ func (p *TracerProxy) GetTransientState(addr common.Address, key common.Hash) co
 	return p.db.GetState(addr, key)
 }
 
-// SelfDestruct an account.
 func (p *TracerProxy) SelfDestruct(address common.Address) uint256.Int {
-	// register event
 	p.ctx.WriteAddressOp(SelfDestructID, &address)
-
-	// call real StateDB
 	return p.db.SelfDestruct(address)
 }
 
-// HasSelfDestructed checks whether a contract has been suicided.
 func (p *TracerProxy) HasSelfDestructed(address common.Address) bool {
-	// register event
 	p.ctx.WriteAddressOp(HasSelfDestructedID, &address)
-
-	// call real StateDB
 	return p.db.HasSelfDestructed(address)
 }
 
-// Exist checks whether the contract exists in the StateDB.
 func (p *TracerProxy) Exist(address common.Address) bool {
-	// register event
 	p.ctx.WriteAddressOp(ExistID, &address)
-
-	// call real StateDB
 	return p.db.Exist(address)
 }
 
-// Empty checks whether the contract is either non-existent
-// or empty according to the EIP161 specification (balance = nonce = code = 0).
 func (p *TracerProxy) Empty(address common.Address) bool {
-	// register event
 	p.ctx.WriteAddressOp(EmptyID, &address)
-
-	// call real StateDB
 	return p.db.Empty(address)
 }
 
-// Prepare handles the preparatory steps for executing a state transition.
 func (p *TracerProxy) Prepare(rules params.Rules, sender, coinbase common.Address, dest *common.Address, precompiles []common.Address, txAccesses types.AccessList) {
-	// call real StateDB
 	p.db.Prepare(rules, sender, coinbase, dest, precompiles, txAccesses)
 }
 
-// AddAddressToAccessList adds an address to the access list.
 func (p *TracerProxy) AddAddressToAccessList(address common.Address) {
-	// call real StateDB
 	p.db.AddAddressToAccessList(address)
 }
 
-// AddressInAccessList checks whether an address is in the access list.
 func (p *TracerProxy) AddressInAccessList(address common.Address) bool {
-	// call real StateDB
 	return p.db.AddressInAccessList(address)
 }
 
-// SlotInAccessList checks whether the (address, slot)-tuple is in the access list.
 func (p *TracerProxy) SlotInAccessList(address common.Address, slot common.Hash) (bool, bool) {
-	// call real StateDB
 	return p.db.SlotInAccessList(address, slot)
 }
 
-// AddSlotToAccessList adds the given (address, slot)-tuple to the access list
 func (p *TracerProxy) AddSlotToAccessList(address common.Address, slot common.Hash) {
-	// call real StateDB
 	p.db.AddSlotToAccessList(address, slot)
 }
 
-// RevertToSnapshot reverts all state changes from a given revision.
 func (p *TracerProxy) RevertToSnapshot(snapshot int) {
-	// register event
 	p.ctx.WriteOp(RevertToSnapshotID)
-
-	// find snapshot
-	for i, recordedSnapshot := range p.snapshots {
-		if recordedSnapshot == snapshot {
-			// register snapshot delta
-			p.ctx.WriteSnapshotDelta(len(p.snapshots) - i - 1)
-
-			// cull all elements between found snapshot and top-of-stack
-			p.snapshots = p.snapshots[0:i]
-			break
-		}
-	}
-
-	// call real StateDB
 	p.db.RevertToSnapshot(snapshot)
 }
 
-// Snapshot returns an identifier for the current revision of the state.
 func (p *TracerProxy) Snapshot() int {
-	// register event
 	p.ctx.WriteOp(SnapshotID)
-
-	// call real StateDB
-	snapshot := p.db.Snapshot()
-
-	// add snapshot
-	p.snapshots = append(p.snapshots, snapshot)
-
 	return snapshot
 }
 
-// AddLog adds a log entry.
 func (p *TracerProxy) AddLog(log *types.Log) {
-	// call real StateDB
 	p.db.AddLog(log)
 }
 
-// GetLogs retrieves log entries.
 func (p *TracerProxy) GetLogs(hash common.Hash, block uint64, blockHash common.Hash, blkTimestamp uint64) []*types.Log {
-	// call real StateDB
 	return p.db.GetLogs(hash, block, blockHash, blkTimestamp)
 }
 
-// PointCache returns the point cache used in computations.
 func (p *TracerProxy) PointCache() *utils.PointCache {
 	return p.db.PointCache()
 }
 
-// Witness retrieves the current state witness.
 func (p *TracerProxy) Witness() *stateless.Witness {
 	return p.db.Witness()
 }
 
-// AddPreimage adds a SHA3 preimage.
 func (p *TracerProxy) AddPreimage(address common.Hash, image []byte) {
-	// call real StateDB
 	p.db.AddPreimage(address, image)
 }
 
@@ -321,27 +208,19 @@ func (p *TracerProxy) AccessEvents() *geth_state.AccessEvents {
 	return p.db.AccessEvents()
 }
 
-// SetTxContext sets the current transaction hash and index.
 func (p *TracerProxy) SetTxContext(thash common.Hash, ti int) {
-	// call real StateDB
 	p.db.SetTxContext(thash, ti)
 }
 
-// Finalise the state in StateDB.
 func (p *TracerProxy) Finalise(deleteEmptyObjects bool) {
-	// call real StateDB
 	p.db.Finalise(deleteEmptyObjects)
 }
 
-// IntermediateRoot computes the current hash of the StateDB.
 func (p *TracerProxy) IntermediateRoot(deleteEmptyObjects bool) common.Hash {
-	// call real StateDB
 	return p.db.IntermediateRoot(deleteEmptyObjects)
 }
 
-// Commit StateDB
 func (p *TracerProxy) Commit(block uint64, deleteEmptyObjects bool) (common.Hash, error) {
-	// call real StateDB
 	return p.db.Commit(block, deleteEmptyObjects)
 }
 
@@ -353,9 +232,7 @@ func (p *TracerProxy) Error() error {
 	return p.db.Error()
 }
 
-// GetSubstatePostAlloc gets substate post allocation.
 func (p *TracerProxy) GetSubstatePostAlloc() txctx.WorldState {
-	// call real StateDB
 	return p.db.GetSubstatePostAlloc()
 }
 
@@ -364,62 +241,38 @@ func (p *TracerProxy) PrepareSubstate(substate txctx.WorldState, block uint64) {
 }
 
 func (p *TracerProxy) BeginTransaction(number uint32) error {
-	// register event
 	p.ctx.WriteOp(BeginTransactionID)
-
-	// call real StateDB
 	if err := p.db.BeginTransaction(number); err != nil {
 		return err
 	}
-
-	// clear all snapshots
-	p.snapshots = []int{}
 	return nil
 }
 
 func (p *TracerProxy) EndTransaction() error {
-	// register event
 	p.ctx.WriteOp(EndTransactionID)
-
-	// call real StateDB
 	if err := p.db.EndTransaction(); err != nil {
 		return err
 	}
-
-	// clear all snapshots
-	p.snapshots = []int{}
 	return nil
 }
 
 func (p *TracerProxy) BeginBlock(number uint64) error {
-	// register event
 	p.ctx.WriteOp(BeginBlockID)
-
-	// call real StateDB
 	return p.db.BeginBlock(number)
 }
 
 func (p *TracerProxy) EndBlock() error {
-	// register event
 	p.ctx.WriteOp(EndBlockID)
-
-	// call real StateDB
 	return p.db.EndBlock()
 }
 
 func (p *TracerProxy) BeginSyncPeriod(number uint64) {
-	// register event
 	p.ctx.WriteOp(BeginSyncPeriodID)
-
-	// call real StateDB
 	p.db.BeginSyncPeriod(number)
 }
 
 func (p *TracerProxy) EndSyncPeriod() {
-	// register event
 	p.ctx.WriteOp(EndSyncPeriodID)
-
-	// call real StateDB
 	p.db.EndSyncPeriod()
 }
 
@@ -453,11 +306,11 @@ func (p *TracerProxy) CreateContract(addr common.Address) {
 }
 
 func (p *TracerProxy) SelfDestruct6780(addr common.Address) (uint256.Int, bool) {
-	p.ctx.WriteOp(SelfDestruct6780ID)
+	p.ctx.WriteAddressOp(SelfDestruct6780ID, addr)
 	return p.db.SelfDestruct6780(addr)
 }
 
 func (p *TracerProxy) GetStorageRoot(addr common.Address) common.Hash {
-	p.ctx.WriteOp(CreateContractID)
+	p.ctx.WriteAddressOp(CreateContractID, addr)
 	return p.db.GetStorageRoot(addr)
 }
