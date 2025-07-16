8c89ae8b039f2b1d862de7f3c79b1243de29613d
cabrador <84449820+cabrador@users.noreply.github.com>
2025-07-16 10:53:17 +0200

    Fix Prague ethtests (#106)

--- ethtest/block_enviroment.go
+++ ethtest/block_enviroment.go
@@ -45,18 +45,12 @@ func (s *stBlockEnvironment) GetCoinbase() common.Address {
 
 func (s *stBlockEnvironment) GetBlobBaseFee() *big.Int {
 	if s.chainCfg.IsCancun(new(big.Int), s.Timestamp.Uint64()) && s.ExcessBlobGas != nil {
-		cancunTime := uint64(0)
-		config := &params.ChainConfig{}
-		config.LondonBlock = big.NewInt(0)
-		config.CancunTime = &cancunTime
-		config.BlobScheduleConfig = &params.BlobScheduleConfig{
-			Cancun: params.DefaultCancunBlobConfig,
-		}
 		excessBlobGas := s.ExcessBlobGas.Uint64()
 		header := &types.Header{
 			ExcessBlobGas: &excessBlobGas,
+			RequestsHash:  &types.EmptyRequestsHash,
 		}
-		return eip4844.CalcBlobFee(config, header)
+		return eip4844.CalcBlobFee(s.chainCfg, header)
 	}
 
 	return nil
--- ethtest/transaction_test.go
+++ ethtest/transaction_test.go
@@ -1,11 +1,14 @@
 package ethtest
 
 import (
+	"encoding/json"
+	"errors"
 	"math/big"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -16,6 +19,14 @@ func TestStTransaction_ToMessage(t *testing.T) {
 		TxBytes:         hexutil.Bytes{0x01, 0x02},
 		ExpectException: "err",
 		Indexes:         Index{Data: 0, Gas: 0, Value: 0},
+	}
+	stAuth := stAuthorization{
+		ChainID: big.NewInt(1),
+		Address: common.HexToAddress("0x9abc"),
+		Nonce:   1,
+		V:       27,
+		R:       big.NewInt(123456789),
+		S:       big.NewInt(987654321),
 	}
 	to := common.HexToAddress("0x1234")
 	bytesTo, err := to.MarshalText()
@@ -31,6 +42,9 @@ func TestStTransaction_ToMessage(t *testing.T) {
 		MaxPriorityFeePerGas: newBigInt(1),
 		BlobGasFeeCap:        newBigInt(1),
 		To:                   string(bytesTo),
+		AuthorizationList: []*stAuthorization{
+			&stAuth,
+		},
 	}
 	msg, err := st.toMessage(post, newBigInt(1))
 	assert.NoError(t, err)
@@ -39,6 +53,7 @@ func TestStTransaction_ToMessage(t *testing.T) {
 	assert.Equal(t, st.BlobGasFeeCap.Convert(), msg.BlobGasFeeCap)
 	assert.Equal(t, []byte{0x12, 0x34}, msg.Data)
 	assert.Equal(t, st.Value[0], msg.Value.String())
+	assert.Equal(t, *st.AuthorizationList[0], stAuth)
 }
 
 func TestStTransaction_ToMessageError(t *testing.T) {
@@ -216,4 +231,103 @@ func TestEthTest_bigMin(t *testing.T) {
 	assert.Equal(t, a, bigMin(a, b))
 	assert.Equal(t, a, bigMin(b, a))
 	assert.Equal(t, b, bigMin(b, b))
+}
+
+func TestStTransaction_MarshallAndUnmarshall(t *testing.T) {
+	sender := common.HexToAddress("0x5678")
+	st := stTransaction{
+		Data:                 []string{"0x1234"},
+		Value:                []string{"1234"},
+		GasLimit:             []*BigInt{newBigInt(1000000)},
+		GasPrice:             newBigInt(1),
+		Nonce:                newBigInt(2),
+		MaxFeePerGas:         newBigInt(3),
+		MaxPriorityFeePerGas: newBigInt(4),
+		BlobGasFeeCap:        newBigInt(5),
+		To:                   "0x1234",
+		AccessLists: []*types.AccessList{
+			{
+				{
+					Address:     common.HexToAddress("0x1234"),
+					StorageKeys: []common.Hash{common.HexToHash("0x5678")},
+				},
+			},
+		},
+		PrivateKey: hexutil.MustDecode("0x1234"),
+		Sender:     &sender,
+		AuthorizationList: []*stAuthorization{
+			{
+				ChainID: big.NewInt(1),
+				Address: common.HexToAddress("0x9abc"),
+				Nonce:   1,
+				V:       27,
+				R:       big.NewInt(123456789),
+				S:       big.NewInt(987654321),
+			},
+		},
+		BlobHashes: []common.Hash{
+			common.HexToHash("0xabcdef"),
+		},
+	}
+
+	marshal, err := json.Marshal(st)
+	assert.NoError(t, err)
+	var unmarshalled stTransaction
+	err = json.Unmarshal(marshal, &unmarshalled)
+	assert.NoError(t, err)
+
+	assert.Equal(t, st, unmarshalled)
+}
+
+func TestStAuthorization_NothingMustBeNil(t *testing.T) {
+	tests := []struct {
+		name    string
+		auth    stAuthorization
+		wantErr error
+	}{
+		{
+			name: "ChainID is nil",
+			auth: stAuthorization{
+				Address: common.HexToAddress("0x9abc"),
+				Nonce:   1,
+				V:       27,
+				R:       big.NewInt(123456789),
+				S:       big.NewInt(987654321),
+			},
+			wantErr: errors.New("missing required field 'chainId' for stAuthorization"),
+		},
+		{
+			name: "R is nil",
+			auth: stAuthorization{
+				ChainID: big.NewInt(1),
+				Address: common.HexToAddress("0x9abc"),
+				Nonce:   1,
+				S:       big.NewInt(987654321),
+			},
+			wantErr: errors.New("missing required field 'r' for stAuthorization"),
+		},
+		{
+			name: "S is nil",
+			auth: stAuthorization{
+				ChainID: big.NewInt(1),
+				Address: common.HexToAddress("0x9abc"),
+				Nonce:   1,
+				V:       27,
+				R:       big.NewInt(123456789),
+			},
+			wantErr: errors.New("missing required field 's' for stAuthorization"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			marshal, err := test.auth.MarshalJSON()
+			assert.NoError(t, err)
+
+			var unmarshalled stAuthorization
+			err = json.Unmarshal(marshal, &unmarshalled)
+			assert.Error(t, err)
+			assert.Equal(t, test.wantErr, err)
+		})
+	}
 }
