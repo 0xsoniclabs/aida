dceab99c9f5932bc0e59cca0ac4a874d36fb98e4
cabrador <ph@fantom.foundation>
2025-07-24 09:25:16 +0000

    Check error and close the context

--- executor/extension/statedb/tracer_proxy_prepper.go
+++ executor/extension/statedb/tracer_proxy_prepper.go
@@ -66,3 +66,11 @@ func (p *tracerProxyPrepper[T]) PreTransaction(_ executor.State[T], ctx *executo
 	ctx.State = proxy.NewTracerProxy(ctx.State, p.ctx)
 	return nil
 }
+func (p *tracerProxyPrepper[T]) PostTransaction(_ executor.State[T], ctx *executor.Context) error {
+	// Check if the proxy produced any errors
+	return ctx.State.Error()
+}
+
+func (p *tracerProxyPrepper[T]) PostRun(_ executor.State[T], ctx *executor.Context, _ error) error {
+	return p.ctx.Close()
+}
