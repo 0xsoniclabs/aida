6bb5d7088d72c4f2ade0a55229fbb17971ccbccb
cabrador <ph@fantom.foundation>
2025-07-24 09:25:16 +0000

    Check error and close the context

--- executor/extension/statedb/tracer_proxy_prepper_test.go
+++ executor/extension/statedb/tracer_proxy_prepper_test.go
@@ -17,7 +17,11 @@
 package statedb
 
 import (
+	"github.com/0xsoniclabs/aida/state"
+	"github.com/0xsoniclabs/aida/tracer"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 	"testing"
 
 	"github.com/0xsoniclabs/aida/executor"
@@ -35,15 +39,46 @@ func TestTemporaryProxyRecorderPrepper_PreTransaction_CreatesProxy(t *testing.T)
 	ctx := &executor.Context{}
 
 	err := p.PreRun(executor.State[any]{}, ctx)
-	if err != nil {
-		t.Fatalf("unexpected error; %v", err)
-	}
+	require.NoError(t, err)
 
 	err = p.PreTransaction(executor.State[any]{}, ctx)
-	if err != nil {
-		t.Fatalf("unexpected error; %v", err)
-	}
+	require.NoError(t, err)
 
 	_, ok := ctx.State.(*proxy.TracerProxy)
 	assert.True(t, ok, "Proxy was not created in PreRun")
+}
+
+func TestTemporaryProxyRecorderPrepper_PostTransaction_ChecksForErrors(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	db := state.NewMockStateDB(ctrl)
+	db.EXPECT().Error()
+
+	cfg := &utils.Config{
+		TraceFile: t.TempDir() + "test_trace",
+	}
+
+	p := MakeTracerProxyPrepper[any](cfg)
+
+	ctx := &executor.Context{
+		State: db,
+	}
+	err := p.PostTransaction(executor.State[any]{}, ctx)
+	require.NoError(t, err)
+}
+
+func TestTemporaryProxyRecorderPrepper_PostRun_ClosesCtx(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	argCtx := tracer.NewMockArgumentContext(ctrl)
+	argCtx.EXPECT().Close()
+
+	cfg := &utils.Config{
+		TraceFile: t.TempDir() + "test_trace",
+	}
+
+	p := makeTracerProxyPrepper[any](cfg)
+	p.ctx = argCtx
+
+	ctx := &executor.Context{}
+	err := p.PostRun(executor.State[any]{}, ctx, nil)
+	require.NoError(t, err)
 }
