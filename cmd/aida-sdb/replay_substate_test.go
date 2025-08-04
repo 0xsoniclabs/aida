7b4f2a5017d84f2d555bb559c12a70bba342f949
cabrador <ph@fantom.foundation>
2025-08-04 09:30:00 +0000

    Add replay tests, rm trace folder

--- cmd/aida-sdb/replay_substate_test.go
+++ cmd/aida-sdb/replay_substate_test.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Aida. If not, see <http://www.gnu.org/licenses/>.
 
-package trace
+package main
 
 // todo: will be handled in upcoming PR
 //
