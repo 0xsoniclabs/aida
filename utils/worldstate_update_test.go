1ae64198f77c00a47e67ed834a10fa042a408626
cabrador <ph@fantom.foundation>
2025-08-13 08:00:01 +0000

    Formattiing

--- utils/worldstate_update_test.go
+++ utils/worldstate_update_test.go
@@ -1,8 +1,9 @@
 package utils
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	"github.com/0xsoniclabs/substate/db"
 	"github.com/0xsoniclabs/substate/rlp"
