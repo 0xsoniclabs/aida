646b8245c9505e4b32d4f9793a5a898fa525bdda
cabrador <ph@fantom.foundation>
2025-08-21 08:53:27 +0000

    Formatting

--- cmd/util-db/merge/merge_test.go
+++ cmd/util-db/merge/merge_test.go
@@ -33,7 +33,7 @@ func TestMerge_Command(t *testing.T) {
 	err = sdb2.Close()
 	require.NoError(t, err)
 
-	_, aidaDbPath := utils.CreateTestSubstateDb(t)
+	_, aidaDbPath := utils.CreateTestSubstateDb(t, db.ProtobufEncodingSchema)
 	app := cli.NewApp()
 	app.Action = mergeAction
 	app.Flags = Command.Flags
