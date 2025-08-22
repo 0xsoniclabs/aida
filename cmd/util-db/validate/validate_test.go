733c77af62aa67c0c2b9b61783ab98d61a05886c
cabrador <ph@fantom.foundation>
2025-08-22 08:37:12 +0000

    Use args builder

--- cmd/util-db/validate/validate_test.go
+++ cmd/util-db/validate/validate_test.go
@@ -13,7 +13,7 @@ import (
 
 func TestCmd_ValidateCommand(t *testing.T) {
 	// given
-	_, aidaDbPath := utils.CreateTestSubstateDb(t)
+	_, aidaDbPath := utils.CreateTestSubstateDb(t, db.ProtobufEncodingSchema)
 	app := cli.NewApp()
 	app.Commands = []*cli.Command{&Command}
 
@@ -93,7 +93,7 @@ func TestCmd_ValidateCommandError(t *testing.T) {
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			_, aidaDbPath := utils.CreateTestSubstateDb(t)
+			_, aidaDbPath := utils.CreateTestSubstateDb(t, db.ProtobufEncodingSchema)
 			test.setup(aidaDbPath)
 			app := cli.NewApp()
 			app.Commands = []*cli.Command{&Command}
