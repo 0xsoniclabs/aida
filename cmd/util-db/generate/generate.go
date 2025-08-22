dbfa80d11aeec6889080b81112bdbcd46822117f
cabrador <ph@fantom.foundation>
2025-08-22 08:01:48 +0000

    Address reviewers feedback

--- cmd/util-db/generate/generate.go
+++ cmd/util-db/generate/generate.go
@@ -5,7 +5,7 @@ import "github.com/urfave/cli/v2"
 // Command is a set of subcommands for generating various database-related stuff.
 var Command = cli.Command{
 	Name:  "generate",
-	Usage: `Used for generating data.`,
+	Usage: `Generates precompute substate data.`,
 	Subcommands: []*cli.Command{
 		&generateDbHashCommand,
 		&generateDeletedAccountsCommand,
