41ac358b453313223ebe491ab29e262392bb7b89
Bernhard Scholz <berhard@fantom.foundation>
2025-08-26 15:17:42 +1000

    Add new interface

--- stochastic/generator/random_access.go
+++ stochastic/generator/random_access.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Fantom Foundation
+// Copyright 2025 Fantom Foundation
 // This file is part of Aida Testing Infrastructure for Sonic
 //
 // Aida is free software: you can redistribute it and/or modify
@@ -25,133 +25,114 @@ import (
 	"github.com/0xsoniclabs/aida/stochastic/statistics"
 )
 
-// minRandomAccessSize must be substantially larger than statistics.QueueLen
+// minSize must be substantially larger than statistics.QueueLen
 // (Otherwise sampling for arguments with class RandomValueID may
 // take a very long time and would slow down the simulation.)
-const minRandomAccessSize = 10 * statistics.QueueLen
+const minSize = 100 * statistics.QueueLen
+
+
+// Randomizer interface 
+type Randomizer interface {
+        RandRange(l int64, u int64) int64  // produce a random value in the interval [l,u]
+        Sample(n int64) int64              // sample distribution
+        SampleQueue() int                  // sample queue distribution
+}
 
 // RandomAccess data structure for producing random index accesses.
 type RandomAccess struct {
-	// cardinality of set
-	numElem int64
-
-	// lambda parameter of exponential distribution
-	lambda float64
-
-	// queue for indexes (always fixed to length qStatslen)
-	// Note that elements in queue are stored in the range from 0 to numElem-1
-	// and need to be shifted by one due to the zero value.
-	queue []int64
-
-	// probability distribution of queue for selecting recent values.
-	qpdf []float64
-
-	// random generator
-	rg *rand.Rand
+	n int64         // cardinality of set
+	queue []int64   // queue for indexes whose length is less than qStatslen
+	rand Randomizer // random generator
 }
 
-// NewAccess creates a new access index.
-func NewRandomAccess(rg *rand.Rand, numElem int64, lambda float64, qpdf []float64) *RandomAccess {
-	if numElem < minRandomAccessSize {
-		return nil
-	}
-
-	// fill queue with uniform random indexes.
+// NewAccess creates a new random access instance for arguments
+func NewRandomAccess(n int64, rand Randomizer) *RandomAccess {
 	queue := []int64{}
 	for i := 0; i < statistics.QueueLen; i++ {
-		queue = append(queue, rg.Int63n(numElem))
+		queue = append(queue, rand.RandRange(1,n))
 	}
-
-	// create a copy of the queue distribution.
-	copyQpdf := make([]float64, statistics.QueueLen)
-	copy(copyQpdf, qpdf)
-
 	return &RandomAccess{
-		numElem: numElem,
-		lambda:  lambda,
-		queue:   queue,
-		qpdf:    copyQpdf,
-		rg:      rg,
+		n:     n,
+		queue: queue,
+		rand:  rand,
 	}
 }
 
-// NextIndex returns the next random index based on the provided class.
-func (a *RandomAccess) NextIndex(class int) int64 {
+// NextIndex returns the next random value for the given argument class
+func (a *RandomAccess) NextIndex(class int) (int64, error) {
 	switch class {
 
 	case statistics.NoArgID:
-		return -1
+		return 0, fmt.Errorf("NextIndex: illegal invocation for no-argument class")
 
+	// only way to return zero value/all other access classes
+	// will result in a non-zero result.
 	case statistics.ZeroValueID:
-		// only way to return zero value/all other access classes
-		// will result in a non-zero result.
-		return 0
+		return 0, nil
 
+	// increment population size of access set
+	// and return newly introduced element.
 	case statistics.NewValueID:
-		// increment population size of access set
-		// and return newly introduced element.
-		if a.numElem == math.MaxInt64 {
-			return -1
+		if a.n == math.MaxInt64 {
+			return 0, fmt.Errorf("NextIndex: new value exceeds cardinality range")
 		}
-		v := a.numElem
+		v := a.n
 		a.placeQ(v)
-		a.numElem++
-		return v + 1
+		a.n ++
+		return v + 1, nil
 
+	// use randomised value that is not contained in the queue
 	case statistics.RandomValueID:
-		// use randomised value that is not contained in the queue
 		for {
-			v := exponential.DiscreteSample(a.rg, a.lambda, a.numElem)
+			v := a.rand.Sample(a.n-1)
 			if !a.findQElem(v) {
 				a.placeQ(v)
-				return v + 1
+				return v + 1, nil
 			}
 		}
 
+	// return the value of the first position in the queue
 	case statistics.PreviousValueID:
-		// return the value of the first position in the queue
 		v := a.lastQ()
 		a.placeQ(v)
-		return v + 1
+		return v + 1, nil
 
+	// return the first position in the queue
 	case statistics.RecentValueID:
-		if v := a.recentQ(); v != -1 {
+		if v, err := a.recentQ(); err != nil {
 			a.placeQ(v)
 			return v + 1
 		} else {
-			return -1
+			return err
 		}
 
 	default:
-		return -1
+		return 0, fmt.Errorf("Unknown argument class")
 	}
 }
 
-// DeleteIndex deletes an access index.
+// DeleteIndex deletes a value reducing the cardinality by one
 func (a *RandomAccess) DeleteIndex(v int64) error {
 	// check index range
-	if v < 0 || v >= a.numElem {
+	if v < 0 || v >= a.n {
 		return fmt.Errorf("DeleteIndex: index (%v) out of index range", v)
 	}
-
 	// reduce cardinality by one
-	a.numElem--
-	if a.numElem < minRandomAccessSize {
+	if a.n <= minSize {
 		return fmt.Errorf("DeleteIndex: cardinality of set too low")
 	}
-
+	a.n--
 	// replace deleted last element by new element
 	// note that the actual deleted element may be
 	// in range, but there might elements in the queue
 	// that exceed the new range limit. They need to
 	// be replaced.
-	j := exponential.DiscreteSample(a.rg, a.lambda, a.numElem)
+	j := rand.Sample(a.n-1)
 	for i := 0; i < statistics.QueueLen; i++ {
-		if a.queue[i] >= a.numElem {
+		if a.queue[i] >= a.n {
 			a.queue[i] = j
 		}
 	}
-
 	return nil
 }
 
@@ -165,38 +146,6 @@ func (a *RandomAccess) findQElem(elem int64) bool {
 	return false
 }
 
-// getRandQPos obtains the next queue position.
-// TODO: Consider replacing the pdf with an
-// exponential distribution. A new value can be
-// produced much faster.
-func (a *RandomAccess) getRandQPos() int {
-	// obtain random number in [0, 1.0)
-	r := a.rg.Float64()
-
-	// compute inverse CDF and select the index
-	sum := float64(0)
-	c := float64(0)
-	factor := 1.0 - a.qpdf[0]
-	j := -1
-	// skip first slot (only used for previousValue)
-	// use Kahan's sum for avoiding numerical issues.
-	for i := 1; i < statistics.QueueLen; i++ {
-		y := (a.qpdf[i] / factor) - c
-		t := sum + y
-		c = (t - sum) - y
-		sum = t
-		if r <= sum {
-			return i
-		}
-		// if numerically unstable, return last
-		// non-zero entry as a solution.
-		if a.qpdf[i] > 0.0 {
-			j = i
-		}
-	}
-	return j
-}
-
 // placeQ places element in the queue.
 func (a *RandomAccess) placeQ(elem int64) {
 	a.queue = append([]int64{elem}, a.queue[0:statistics.QueueLen-1]...)
@@ -208,14 +157,10 @@ func (a *RandomAccess) lastQ() int64 {
 }
 
 // recentQ returns some element in the queue but not the previous one.
-func (a *RandomAccess) recentQ() int64 {
-	i := a.getRandQPos()
-	switch i {
-	case 0:
-		panic("getRandPos() returned previous element.")
-	case -1:
-		return -1
-	default:
-		return a.queue[i]
-	}
+func (a *RandomAccess) recentQ() (int64, error) {
+	i := a.rand.SampleQueue()
+	if (i <= 0 || i >= statistics.QueueLen)  {
+		return 0, fmt.Errorf("recentQ: queue index out of range for recent access")
+        } 
+	return a.queue[i]
 }
